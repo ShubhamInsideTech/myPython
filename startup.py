5cec784cf16586a6ac54976abaf20548ee5286da
ShubhamInsideTech <35133870+ShubhamInsideTech@users.noreply.github.com>
2018-10-29 09:18:58 +0530

    Import functionality

--- startup.py
+++ startup.py
@@ -8,3 +8,19 @@ for i in range(1,10,1):
         continue
     else:
         print (i)
+
+        
+import array as arr
+vals = arr.array('i',[5,9,4,5,6,8,1])
+for i in range(len(vals)):
+    print (vals[i])
+for e in vals:
+print(e)
+
+import array as arr
+from array import *
+ar1 = arr.array('i',[])
+n = int(input("Pleas enter the length of Array : "))
+for i in range(n):
+    i=int(input("Enter the element : "))
+    ar1.append(i)

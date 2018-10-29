ef279923dfc6c329541ef74a2921ba22f0ea912c
ShubhamInsideTech <35133870+ShubhamInsideTech@users.noreply.github.com>
2018-10-29 09:12:49 +0530

    if loop

--- startup.py
+++ startup.py
@@ -2,3 +2,9 @@ x=['shubham',65,2.5]
 for i in range(12,20,1):
     if i%5 == 0:
         print (i)
+
+for i in range(1,10,1):
+    if i%3==0 or i%5==0:
+        continue
+    else:
+        print (i)
